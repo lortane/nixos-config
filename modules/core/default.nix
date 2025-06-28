c8a7dfbea75bafc0d91b4170f90ee92bdf7f53a5
lortane <lortane@pm.me>
2025-06-28 22:12:42 +0200

    Install steam

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -9,6 +9,7 @@
     ++ [ (import ./program.nix) ]
     ++ [ (import ./security.nix) ]
     ++ [ (import ./services.nix) ]
+    ++ [ (import ./steam.nix) ]
     ++ [ (import ./system.nix) ]
     ++ [ (import ./user.nix) ]
     ++ [ (import ./wayland.nix) ]
